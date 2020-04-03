9c438d018a038fb89c9c7ce73ae75dd954606f68
Kumar Kartikay <krkartikay@gmail.com>
2020-04-03 11:23:01 +0530

    add argparser

--- graph.py
+++ graph.py
@@ -1,9 +1,19 @@
 import time
 import threading
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
 lgd = []
 data = []
+
+parser = ArgumentParser(
+    prog="graph.py",
+    description="Generate animated graph of incoming csv data"
+)
+parser.add_argument("--timedelay", "-t", default=0.1, type=float)
+
+args = parser.parse_args()
+
 
 def main():
     thread1 = threading.Thread(target=input_thread, name="Input thread")
@@ -30,13 +40,12 @@ def plot_thread():
     plt.style.use("seaborn")
     fig = plt.figure()
     plt.legend()
-    print(lgd)
     while True:
         for i in range(len(lgd)):
             plt.plot(data[i], linewidth=1, label=lgd[i])
         plt.legend()
         plt.draw()
-        time.sleep(0.1)
+        time.sleep(args.timedelay)
         fig.canvas.flush_events()
         plt.clf()
 
